23ea45ad55bdd6424dcf862e5daab227961eb028
Manneaber <Manneaber@users.noreply.github.com>
2026-04-16 09:45:31 +0000

    refactor: enhance user input handling in config flow for MEA Electricity Tariffs

--- custom_components/mea-electricity-tariffs/config_flow.py
+++ custom_components/mea-electricity-tariffs/config_flow.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+
 from .const import DOMAIN
 
 
@@ -9,9 +12,28 @@ class MeaElectricityTariffsFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(
+        self,
+        user_input: dict | None = None,
+    ) -> config_entries.ConfigFlowResult:
         """Handle the initial step."""
         if self._async_current_entries():
             return self.async_abort(reason="already_configured")
 
-        return self.async_create_entry(title="MEA Electricity Tariffs", data={})
+        if user_input is not None:
+            return self.async_create_entry(
+                title=user_input[CONF_NAME],
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_NAME,
+                        default="MEA Electricity Tariffs",
+                    ): str,
+                }
+            ),
+        )
